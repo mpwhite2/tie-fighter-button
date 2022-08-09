2448d909098e9dd98fa4dfc4f6ed882358629a2f
mpwhite2 <110784371+mpwhite2@users.noreply.github.com>
2022-08-08 19:28:55 -0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,17 +1,52 @@
+random = 0
+Y_Acceleration = 0
+X_Acceleration = 0
 laser = None
-tie = None
+tie = game.create_sprite(0, 0)
+game.start_countdown(60000)
 
 def on_forever():
+    global X_Acceleration, Y_Acceleration, random
+    X_Acceleration = input.acceleration(Dimension.X)
+    Y_Acceleration = input.acceleration(Dimension.Y)
     game.create_sprite(1, 3)
     game.create_sprite(3, 3)
     game.create_sprite(3, 1)
     game.create_sprite(1, 1)
     
     def on_button_pressed_ab():
-        global laser
+        global laser, tie
         laser = game.create_sprite(2, 2)
         basic.pause(300)
+        if laser.is_touching(tie):
+            tie.delete()
+            game.add_score(10)
+            tie = game.create_sprite(0, 0)
         laser.delete()
     input.on_button_pressed(Button.AB, on_button_pressed_ab)
     
+    # move tie
+    random = randint(0, 3)
+    if random == 0:
+        tie.change(LedSpriteProperty.Y, 1)
+        basic.pause(300)
+    if random == 1:
+        tie.change(LedSpriteProperty.Y, -1)
+        basic.pause(300)
+    if random == 2:
+        tie.change(LedSpriteProperty.X, 1)
+        basic.pause(300)
+    if random == 3:
+        tie.change(LedSpriteProperty.X, -1)
+        basic.pause(300)
+    if X_Acceleration > 120:
+        tie.change(LedSpriteProperty.X, 1)
+    elif X_Acceleration < 120:
+        tie.change(LedSpriteProperty.X, -1)
+    if Y_Acceleration > 120:
+        tie.change(LedSpriteProperty.Y, 1)
+    elif Y_Acceleration < 120:
+        tie.change(LedSpriteProperty.Y, -1)
+    if game.is_game_over() and input.button_is_pressed(Button.A):
+        control.reset()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,21 +1,23 @@
 let random = 0
-let tie = game.createSprite(0, 0)
+let Y_Acceleration = 0
+let X_Acceleration = 0
 let laser = null
+let tie = game.createSprite(0, 0)
 game.startCountdown(60000)
 basic.forever(function () {
+    X_Acceleration = input.acceleration(Dimension.X)
+    Y_Acceleration = input.acceleration(Dimension.Y)
     game.createSprite(1, 3)
 game.createSprite(3, 3)
 game.createSprite(3, 1)
 game.createSprite(1, 1)
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-        
         laser = game.createSprite(2, 2)
         basic.pause(300)
         if (laser.isTouching(tie)){
             tie.delete ()
             game.addScore (10)
             tie = game.createSprite (0,0)
-            
         }
         laser.delete()
     })
@@ -37,13 +39,17 @@ input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
         tie.change(LedSpriteProperty.X, -1)
         basic.pause(300)
     }
-    input.onButtonPressed(Button.A, function() {
-    tie.change (LedSpriteProperty.X,1)
-})
-input.onButtonPressed(Button.B, function() {
-    tie.change (LedSpriteProperty.X,-1)
-})
-if (game.isGameOver() && input.buttonIsPressed(Button.A)) {
+    if (X_Acceleration > 120) {
+        tie.change(LedSpriteProperty.X, 1)
+    } else if (X_Acceleration < 120) {
+        tie.change(LedSpriteProperty.X, -1)
+    }
+    if (Y_Acceleration >120) {
+        tie.change (LedSpriteProperty.Y,1)
+    } else if (Y_Acceleration < 120) {
+        tie.change (LedSpriteProperty.Y,-1)
+    }
+    if (game.isGameOver() && input.buttonIsPressed(Button.A)) {
         control.reset()
     }
 })
